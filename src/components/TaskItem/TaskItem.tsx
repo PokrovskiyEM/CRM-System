ab2b09daa9e3c00a49c8967ddb6ef4875f8baf47
Egor Pokrovskiy <pokrovskiyegor@internet.ru>
2026-03-29 15:19:23 +0300

    removed an unnecessary function

--- src/components/TaskItem/TaskItem.tsx
+++ src/components/TaskItem/TaskItem.tsx
@@ -108,7 +108,6 @@ export const TaskItem = memo(({ todo, onTasksUpdated }: Props) => {
               autoFocus
               onChange={(e) => {
                 setNewTitle(e.target.value)
-                setEditError('')
               }}
             />
             {editError && (
