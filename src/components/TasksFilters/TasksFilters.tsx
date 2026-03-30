1e3f6b1c1e8a461596bbda6df1cc30cd22f1b548
Egor Pokrovskiy <pokrovskiyegor@internet.ru>
2026-03-30 22:48:36 +0300

    replaced TasksFilters with antd components

--- src/components/TasksFilters/TasksFilters.tsx
+++ src/components/TasksFilters/TasksFilters.tsx
@@ -1,5 +1,5 @@
+import { Tabs, type TabsProps } from "antd";
 import type { StatusFilter, TodoInfo } from "../../types/todo";
-import { Tab } from "../../ui-kit/Tab/Tab";
 import styles from "./styles.module.css";
 
 interface Props {
@@ -9,30 +9,33 @@ interface Props {
 }
 
 export const TasksFilters = ({ info, onFilterChange, filter }: Props) => {
-  const changeFilter = (filter: StatusFilter) => {
-    onFilterChange(filter)
+  const handleChange = (key: string) => {
+    onFilterChange(key as StatusFilter)
   }
 
+  const items: TabsProps['items'] = [
+    {
+      key: 'all',
+      label: `Все(${info?.all ?? 0})`,
+    },
+    {
+      key: 'inWork',
+      label: `В работе(${info?.inWork ?? 0})`,
+    },
+    {
+      key: 'completed',
+      label: `Сделано(${info?.completed ?? 0})`,
+    },
+  ];
+
   return (
-    <div className={styles.todoInfo}>
-      <Tab
-        isActive={filter === 'all'}
-        onClick={() => changeFilter('all')}
-      >
-        Все({info?.all})
-      </Tab>
-      <Tab
-        isActive={filter === 'inWork'}
-        onClick={() => changeFilter('inWork')}
-      >
-        В работе({info?.inWork})
-      </Tab>
-      <Tab
-        isActive={filter === 'completed'}
-        onClick={() => changeFilter('completed')}
-      >
-        Сделано({info?.completed})
-      </Tab>
-    </div >
+    <Tabs
+      className={styles.tasksTabs}
+      centered
+      size="large"
+      activeKey={filter}
+      items={items}
+      onChange={handleChange}
+    />
   )
 }
