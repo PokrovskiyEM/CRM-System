e735089188587f13a1d795417e02b003d3a3929a
Egor Pokrovskiy <pokrovskiyegor@internet.ru>
2026-04-02 14:47:53 +0300

    added authApi, userApi and shared api

---
+++ src/api/api.ts
@@ -0,0 +1,77 @@
+import axios, { AxiosError, type AxiosRequestConfig } from "axios";
+import { logout, setAuth } from "../app/slices/authSlice";
+import { store } from "../app/store/store";
+import { tokenManager } from "../helpers/tokenManager";
+
+interface RetryConfig extends AxiosRequestConfig {
+  _retry?: boolean
+}
+
+export const BASE_URL = 'https://easydev.club/api/v1'
+
+export const api = axios.create({
+  baseURL: BASE_URL
+})
+
+api.interceptors.request.use((config) => {
+  const token = tokenManager.getToken()
+  if (token && config.headers) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+})
+
+api.interceptors.response.use(
+  res => res,
+  async (error: AxiosError) => {
+    const status = error.response?.status
+    const originalRequest: RetryConfig | undefined = error.config
+
+    if (!status || !originalRequest) {
+      return Promise.reject(error)
+    }
+
+    if (status !== 401) {
+      return Promise.reject(error)
+    }
+
+    if (originalRequest._retry) {
+      return Promise.reject(error)
+    }
+
+    if (originalRequest.url === '/auth/refresh') {
+      tokenManager.clearToken()
+      localStorage.removeItem('refreshToken')
+      store.dispatch(logout())
+      return Promise.reject(error)
+    }
+
+    originalRequest._retry = true
+
+    const refreshToken = localStorage.getItem('refreshToken')
+    if (!refreshToken) {
+      tokenManager.clearToken()
+      localStorage.removeItem('refreshToken')
+      store.dispatch(logout())
+      return Promise.reject(error)
+    }
+
+    try {
+      const refreshResponse = await api.post('/auth/refresh', { refreshToken })
+
+      const newAccessToken = refreshResponse.data.accessToken
+      tokenManager.setToken(newAccessToken)
+      store.dispatch(setAuth(true))
+
+      originalRequest.headers = originalRequest.headers ?? {}
+
+      originalRequest.headers.Authorization = `Bearer ${newAccessToken}`
+      return api.request(originalRequest)
+    } catch {
+      tokenManager.clearToken()
+      localStorage.removeItem('refreshToken')
+      store.dispatch(logout())
+      return Promise.reject(error)
+    }
+  }
+)
---
+++ src/api/authApi.ts
@@ -0,0 +1,29 @@
+import type { AuthData, RefreshToken, Token, UserRegistration } from "../types/auth";
+import { api } from "./api";
+
+export const signIn = async ({ login, password }: AuthData): Promise<Token> => {
+  const response = await api.post<Token>('/auth/signin', { login, password })
+  return response.data
+}
+
+export const signUp = async ({
+  login,
+  username,
+  password,
+  email,
+  phoneNumber
+}: UserRegistration) => {
+  const response = await api.post('/auth/signup', {
+    login,
+    username,
+    password,
+    email,
+    phoneNumber
+  })
+  return response.status
+}
+
+export const refresh = async (refreshToken: RefreshToken): Promise<Token> => {
+  const response = await api.post<Token>('/auth/refresh', { refreshToken })
+  return response.data
+}
