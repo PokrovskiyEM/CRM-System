63820c736a8b1bfe4a27b0e7205a8a059971308e
Egor Pokrovskiy <pokrovskiyegor@internet.ru>
2026-04-02 11:24:37 +0300

    added redux toolkit and created initial store

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,33 +1,30 @@
-import { BrowserRouter } from "react-router";
-import { AppRouter } from "./router/AppRouter";
 import { Layout } from "antd";
 import { SidebarMenu } from "../components/SidebarMenu/SidebarMenu";
+import { AppRouter } from "./router/AppRouter";
 
 const { Sider, Content } = Layout;
 
 export function App() {
   return (
-    <BrowserRouter>
-      <Layout style={{
-        minHeight: '100vh',
-        background: "var(--color-background)",
-      }}>
-        <Sider
-          theme="light"
-          width={130}
-          style={{
-            position: "sticky",
-            top: 0,
-            height: '100vh',
-            borderRight: "1px solid gray",
-          }}
-        >
-          <SidebarMenu />
-        </Sider>
-        <Content>
-          <AppRouter />
-        </Content>
-      </Layout>
-    </BrowserRouter >
+    <Layout style={{
+      minHeight: '100vh',
+      background: "var(--color-background)",
+    }}>
+      <Sider
+        theme="light"
+        width={130}
+        style={{
+          position: "sticky",
+          top: 0,
+          height: '100vh',
+          borderRight: "1px solid gray",
+        }}
+      >
+        <SidebarMenu />
+      </Sider>
+      <Content>
+        <AppRouter />
+      </Content>
+    </Layout>
   )
 }
