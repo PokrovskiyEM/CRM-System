dec8581434c855cb0c8aa77a77fe61874c956af5
Egor Pokrovskiy <pokrovskiyegor@internet.ru>
2026-04-08 11:54:22 +0300

    added image and changed styles to AuthLayout

--- src/app/layouts/AuthLayout.tsx
+++ src/app/layouts/AuthLayout.tsx
@@ -1,36 +1,22 @@
 import { Layout } from "antd"
 import { Outlet } from "react-router"
+import styles from "./AuthLayout.module.css"
 
 const { Content } = Layout
 
 export const AuthLayout = () => {
   return (
-    <Layout style={{
-      minHeight: '100vh',
-      background: "var(--color-background)",
-    }}>
-      <Content style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-        <div style={{
-          backgroundColor: 'white',
-          borderRadius: 15,
-          padding: 15,
-          width: 500,
-          minHeight: 500,
-          justifyItems: 'center',
-          alignContent: 'center',
-        }}>
-          <div style={{
-            width: '100%',
-            maxWidth: 400
-          }}>
-            <Outlet />
+    <Layout className={styles.layout}>
+      <Content className={styles.content}>
+        <div className={styles.wrapper}>
+          <div className={styles.image} />
+          <div className={styles.form}>
+            <div className={styles.inner}>
+              <Outlet />
+            </div>
           </div>
         </div>
       </Content>
-    </Layout >
+    </Layout>
   )
 }
