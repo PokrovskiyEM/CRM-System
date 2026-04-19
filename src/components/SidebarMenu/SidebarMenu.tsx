a0413308ef983018fda0b12957f35ea78a7e2604
Egor Pokrovskiy <pokrovskiyegor@internet.ru>
2026-04-19 21:58:55 +0300

    removed constant menu_items from the component

--- src/components/SidebarMenu/SidebarMenu.tsx
+++ src/components/SidebarMenu/SidebarMenu.tsx
@@ -1,25 +1,25 @@
 import { useLocation, useNavigate } from "react-router";
 import { Menu } from "antd";
 
+const MENU_ITEMS = [
+  {
+    key: "/todos",
+    label: 'Список задач'
+  },
+  {
+    key: "/profile",
+    label: 'Личный кабинет'
+  },
+]
+
 export const SidebarMenu = () => {
   const navigate = useNavigate();
   const location = useLocation();
 
-  const items = [
-    {
-      key: "/todos",
-      label: 'Список задач'
-    },
-    {
-      key: "/profile",
-      label: 'Личный кабинет'
-    },
-  ]
-
   return (
     <Menu
       selectedKeys={[location.pathname]}
-      items={items}
+      items={MENU_ITEMS}
       onClick={({ key }) => navigate(key)}
     />
   )
