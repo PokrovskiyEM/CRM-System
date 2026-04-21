688fc3b150d91fa7d6efcbe5b461aed75c76a5b3
Egor Pokrovskiy <pokrovskiyegor@internet.ru>
2026-04-21 11:38:29 +0300

    added an anonymous function to the handler

--- src/components/SidebarMenu/SidebarMenu.tsx
+++ src/components/SidebarMenu/SidebarMenu.tsx
@@ -1,26 +1,32 @@
 import { useLocation, useNavigate } from "react-router";
-import { Menu } from "antd";
+import { Menu, type MenuProps } from "antd";
+
+type MenuItem = Required<MenuProps>['items'][number];
+
+const MENU_ITEMS: MenuItem[] = [
+  {
+    key: "/todos",
+    label: 'Список задач'
+  },
+  {
+    key: "/profile",
+    label: 'Профиль'
+  },
+]
 
 export const SidebarMenu = () => {
   const navigate = useNavigate();
   const location = useLocation();
 
-  const items = [
-    {
-      key: "/todos",
-      label: 'Список задач'
-    },
-    {
-      key: "/profile",
-      label: 'Профиль'
-    },
-  ]
+  const handleNavigateTo: MenuProps['onClick'] = ({ key }) => {
+    navigate(key)
+  }
 
   return (
     <Menu
       selectedKeys={[location.pathname]}
-      items={items}
-      onClick={({ key }) => navigate(key)}
+      items={MENU_ITEMS}
+      onClick={handleNavigateTo}
     />
   )
 }
